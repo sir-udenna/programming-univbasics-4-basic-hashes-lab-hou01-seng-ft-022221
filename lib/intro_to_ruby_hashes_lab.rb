af556a09d4def38bfdafd2753238fd4d1922de76
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-06 06:04:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,12 +6,15 @@ end
 def my_hash
   # return a valid hash with any key/value pair of your choice
   hash = {
-    shoe_size: 12
+    shoe_size: 10
   }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {
+    name: 'Grace Hopper'
+  }
 end
 
 
